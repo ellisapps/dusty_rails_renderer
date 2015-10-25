5580b401e49665494b326d8614c58250b5a4cbf9
Admin <dev-admin@devadmin-virtual-machine.(none)>
2015-10-25 16:45:44 -0400

    Update gemspec

--- dusty_rails_renderer.gemspec
+++ dusty_rails_renderer.gemspec
@@ -11,13 +11,13 @@ Gem::Specification.new do |spec|
 
   spec.summary       = "Render Dust server side"
   spec.description   = "Render Dust server side"
-  spec.homepage      = "http://louisellis.com"
+  spec.homepage      = "https://github.com/ellisapps/dusty_rails_renderer"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata['allowed_push_host'] = "https://rubygems.org"
   else
     raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
   end
@@ -28,5 +28,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.10"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rake", "~> 10.0" 
+  spec.add_development_dependency "therubyracer", "~> 0.12.2"
 end
--- lib/dusty_rails_renderer.rb
+++ lib/dusty_rails_renderer.rb
@@ -3,48 +3,48 @@ require "dusty_rails_renderer/version"
 
 module DustyRailsRenderer
   class << self
-  	include Singleton
-   	attr_writer :configuration
+    include Singleton
+    attr_writer :configuration
 
     #Initialize, load Dust.js library, and precompile Dust.js templates
-   	def initialize
-   		@dust_config = YAML.load_file(self.configuration.dust_config_path)
-   		dust_library = File.read(self.configuration.dust_js_library_path)
-	    @precompiled_templates = Hash.new
-   		@context = V8::Context.new
-	    @context.eval(dust_library, 'dustjs') 
+    def initialize
+      @dust_config = YAML.load_file(self.configuration.dust_config_path)
+      dust_library = File.read(self.configuration.dust_js_library_path)
+      @precompiled_templates = Hash.new
+      @context = V8::Context.new
+      @context.eval(dust_library, 'dustjs') 
 
-	    read_dust_files
-   	end
+      read_dust_files
+    end
 
     #Return precompiled templates in JSON format (Client-side)
-	  def templates
-	    @precompiled_templates.to_json
-	  end
+    def templates
+      @precompiled_templates.to_json
+    end
 
     #Render template registered Dust.js template
-   	def render(template_name, json) 
-		  if self.configuration.production
-	  		@context.eval("(function() { var result; dust.render('#{template_name}', #{json}, function(err, out) { result = out; }); return result; })()")
-	  	else 
-	  		read_dust_files
-	  		@context.eval("(function() { var result; dust.render('#{template_name}', #{json}, function(err, out) { result = out; }); return result; })()")
-	  	end
-   	end
+    def render(template_name, json) 
+      if self.configuration.production
+        @context.eval("(function() { var result; dust.render('#{template_name}', #{json}, function(err, out) { result = out; }); return result; })()")
+      else 
+        read_dust_files
+        @context.eval("(function() { var result; dust.render('#{template_name}', #{json}, function(err, out) { result = out; }); return result; })()")
+      end
+    end
 
    	private 
       
-      #Read in and register Dust.js templates
-   	  def read_dust_files
-  	    @dust_config.each do |template, info|
-	      file_url = self.configuration.dust_template_base_path + info["file_path"]
-	      template_name = info["name"]
-	      contents = File.read(file_url).gsub("\n","").gsub("\"","'").gsub("\t","")
-	      template = @context.eval("dust.compile(\"#{contents}\",'#{template_name}')")
-	      @context.eval("dust.loadSource(dust.compile(\"#{contents}\",'#{template_name}'))")
-	      @precompiled_templates[template_name] = template
-  	    end
-      end 
+    #Read in and register Dust.js templates
+    def read_dust_files
+      @dust_config.each do |template, info|
+        file_url = self.configuration.dust_template_base_path + info["file_path"]
+        template_name = info["name"]
+        contents = File.read(file_url).gsub("\n","").gsub("\"","'").gsub("\t","")
+        template = @context.eval("dust.compile(\"#{contents}\",'#{template_name}')")
+        @context.eval("dust.loadSource(dust.compile(\"#{contents}\",'#{template_name}'))")
+        @precompiled_templates[template_name] = template
+      end
+    end 
   end
   
   def self.configure
@@ -62,10 +62,10 @@ module DustyRailsRenderer
     attr_accessor :dust_template_base_path
 
     def initialize
- 	  @dust_config_path = "config/dust_initializer.yml"
-	  @dust_js_library_path = "app/assets/javascripts/libraries/dust/dust-full.js"
-	  @dust_template_base_path = "app/assets/javascripts/dust/"
-	  @production = false	  
+      @dust_config_path = "config/dust_initializer.yml"
+      @dust_js_library_path = "app/assets/javascripts/libraries/dust/dust-full.js"
+      @dust_template_base_path = "app/assets/javascripts/dust/"
+      @production = false	  
     end
   end
 
